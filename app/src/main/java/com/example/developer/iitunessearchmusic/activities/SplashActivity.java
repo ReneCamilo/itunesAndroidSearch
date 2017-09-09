6c6a51686a56076cc39128fcb986179c43611a03
Developer <ing.renecontreras@cenidet.edu.mx>
2017-09-08 20:41:48 -0500

    Adding Splash Screen.

--- app/src/main/java/com/example/developer/iitunessearchmusic/activities/SplashActivity.java
+++ app/src/main/java/com/example/developer/iitunessearchmusic/activities/SplashActivity.java
@@ -1,8 +1,40 @@
 package com.example.developer.iitunessearchmusic.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.developer.iitunessearchmusic.R;
+
 /**
  * Created by developer on 9/8/17.
  */
 
-public class SplashActivity {
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //NetworkConnection.testPath(getString(R.string.base_url));
+        //NetworkConnection.useDecodedUTFInUrl(true);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashActivity.this, Main.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        };
+        thread.start();
+    }
 }
